39cc82828a281924525a08f4dda490f68608a52c
Vika Prokudina <victoriaprokudina76@gmail.com>
2025-12-17 18:16:10 +0500

    feat: add admin dashboard page with CSS and JS

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function () { return view('admin.dashboard'); });
 });
