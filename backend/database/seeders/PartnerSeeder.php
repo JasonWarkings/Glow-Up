713690e74a86ed9366b8982e700fc731a623cb2d
Alina <alinasheleg7@gmail.com>
2026-06-18 20:02:46 +0500

    feat:fix error

--- backend/database/seeders/PartnerSeeder.php
+++ backend/database/seeders/PartnerSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\PartnerRequest;
 
-class PartnerRequestSeeder extends Seeder
+class PartnerSeeder extends Seeder
 {
     public function run(): void
     {
